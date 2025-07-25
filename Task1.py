3e9dfcc2241c4a23a6191fa79f02577db9f95e4a
Ayaankhan224 <ayaankhan224271@gmail.com>
2025-07-25 18:20:35 +0530

    Add files via upload

--- Task1.py
+++ Task1.py
@@ -1,7 +1,9 @@
-int1=int(input('Enter the first number :'))
-int2=int(input('Enter the second number :'))
-
-print('Addition',int1+int2)
-print('Subtraction',int1-int2)
-print('Multiplication',int1*int2)
-print('Division',int1/int2)
+n=int(input('Enter number: '))
+#
+def fact(n):
+    if n < 2:
+        return 1
+    else:
+        return fact(n-1)*n
+#
+print(fact(n))
---
+++ Task2.py
@@ -0,0 +1,6 @@
+n=float(input("Enter number to operate: "))
+#
+from math import sqrt,log,sin
+print("Square Root: ", sqrt(n))
+print("Logartihm: ", log(n))
+print("Sine: ", sin(n))
